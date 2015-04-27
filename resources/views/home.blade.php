d8283eed7f10814c2bb97b9fc1e122ac162c9969
Jimmie <jimmie@define.se>
2015-04-27 17:31:07 +0200

    fix home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,28 @@
 @extends('app')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-10 col-md-offset-1">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Home</div>
 
-                    <div class="panel-body">
-                        You are logged in!
+    <div class="block-top">
+
+        <nav>
+            <ul class="nav">
+                <li><a href="{{ url('/auth/logout') }}">{{ Lang::get('auth.logout') }}</a></li>
+            </ul>
+        </nav>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">{{ Lang::get('default.welcome') }}</div>
+
+                        <div class="panel-body">
+                            {{ Lang::get('default.you_are_signed_in') }}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 @endsection
