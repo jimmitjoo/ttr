e0d0298f9c4c1ccf85276042e7be9d01a596a1fe
Jimmie <jimmie@define.se>
2015-04-28 15:37:25 +0200

    set block top on profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -4,10 +4,12 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>{{ $user->name }}</h1>
+    <div class="block-top">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1>{{ $user->name }}</h1>
+                </div>
             </div>
         </div>
     </div>
