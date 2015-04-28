99ea9ea50e6637a10540ab47ec598ce755caf6c8
Jimmie <jimmie@define.se>
2015-04-28 16:28:02 +0200

    remove form-control class

--- resources/views/race/create.blade.php
+++ resources/views/race/create.blade.php
@@ -16,44 +16,43 @@
 
                     {!! Form::open(['method' => 'post', 'route' => 'saveRace', 'enctype' => 'multipart/form-data']) !!}
 
-                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => Lang::get('race.name')])
+                    {!! Form::text('name', null, ['placeholder' => Lang::get('race.name')])
                     !!}
                     {!! Form::file('logo_src') !!}
                     {!! Form::file('cover_src') !!}
-                    {!! Form::input('date', 'date', date('Y-m-d', strtotime('+2 months')), ['class' =>
-                    'form-control', 'placeholder' =>
+                    {!! Form::input('date', 'date', date('Y-m-d', strtotime('+2 months')), ['placeholder' =>
                     Lang::get('race.date') ]) !!}
-                    {!! Form::text('town', null, ['class' => 'form-control', 'placeholder' => Lang::get('race.town')
+                    {!! Form::text('town', null, ['placeholder' => Lang::get('race.town')
                     ]) !!}
-                    {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::textarea('description', null, ['placeholder' =>
                     Lang::get('race.description') ]) !!}
-                    {!! Form::text('external_link', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('external_link', null, ['placeholder' =>
                     Lang::get('race.external_link') ]) !!}
-                    {!! Form::text('signup_link', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('signup_link', null, ['placeholder' =>
                     Lang::get('race.signup_link')
                     ]) !!}
-                    {!! Form::text('run_name', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('run_name', null, ['placeholder' =>
                     Lang::get('race.run_name')
                     ]) !!}
-                    {!! Form::text('run_distance', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('run_distance', null, ['placeholder' =>
                     Lang::get('race.run_distance')
                     ]) !!}
-                    {!! Form::text('run_entry_fee', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('run_entry_fee', null, ['placeholder' =>
                     Lang::get('race.run_entry_fee')
                     ]) !!}
-                    {!! Form::text('run_late_entry_fee', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('run_late_entry_fee', null, ['placeholder' =>
                     Lang::get('race.run_late_entry_fee')
                     ]) !!}
-                    {!! Form::text('run_first_entry_datetime', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('run_first_entry_datetime', null, ['placeholder' =>
                     Lang::get('race.run_first_entry_datetime')
                     ]) !!}
-                    {!! Form::text('run_first_late_entry_datetime', null, ['class' => 'form-control', 'placeholder'
+                    {!! Form::text('run_first_late_entry_datetime', null, ['placeholder'
                     => Lang::get('race.run_first_late_entry_datetime')
                     ]) !!}
-                    {!! Form::text('run_last_late_entry_datetime', null, ['class' => 'form-control', 'placeholder'
+                    {!! Form::text('run_last_late_entry_datetime', null, ['placeholder'
                     => Lang::get('race.run_last_late_entry_datetime')
                     ]) !!}
-                    {!! Form::text('run_start_datetime', null, ['class' => 'form-control', 'placeholder' =>
+                    {!! Form::text('run_start_datetime', null, ['placeholder' =>
                     Lang::get('race.run_start_datetime')
                     ]) !!}
                     {!! Form::submit( Lang::get('race.save') ) !!}
