29ae9af177470b9826a7770d68bcf9f4745464b8
Jimmie <jimmie@define.se>
2015-04-28 15:36:25 +0200

    setup profile

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,1 +1,15 @@
-<?php dd($user);
+@extends('app')
+
+@section('title', $user->name)
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>{{ $user->name }}</h1>
+            </div>
+        </div>
+    </div>
+
+@endsection
