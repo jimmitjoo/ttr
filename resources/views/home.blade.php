87c6a075516b4a5b191799e061c49f8f49a9d745
Jimmie <jimmie@define.se>
2015-04-28 16:11:17 +0200

    fix logged in nav

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,16 @@
 
     <div class="block-top">
 
-        <nav>
-            <ul class="nav">
-                <li><a href="/lopp/skapa">{{ Lang::get('race.create') }}</a></li>
-                <li><a href="/profil/{{ Auth::getUser()->id }}">{{ Lang::get('auth.profile') }}</a></li>
-                <li><a href="{{ url('/auth/logout') }}">{{ Lang::get('auth.logout') }}</a></li>
-            </ul>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <div id="navbar" class="navbar-collapse collapse">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/lopp/skapa">{{ Lang::get('race.create') }}</a></li>
+                        <li><a href="/profil/{{ Auth::getUser()->id }}">{{ Lang::get('auth.profile') }}</a></li>
+                        <li><a href="{{ url('/auth/logout') }}">{{ Lang::get('auth.logout') }}</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div><!--/.container-fluid -->
         </nav>
 
         <div class="container">
