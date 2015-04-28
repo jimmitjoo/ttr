437441dabe8f1e2ea3bb456a3a56c43facc645d1
Jimmie <jimmie@define.se>
2015-04-28 16:20:06 +0200

    menu

--- resources/views/auth/menu.blade.php
+++ resources/views/auth/menu.blade.php
@@ -3,6 +3,8 @@
         <div class="container-fluid">
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav">
+                    <li><a href="/">{{ Lang::get('default.start') }}</a></li>
+                    <li><a href="/hem">{{ Lang::get('default.dashboard') }}</a></li>
                     <li><a href="/lopp/skapa">{{ Lang::get('race.create') }}</a></li>
                     @if (Auth::getUser()->id)
                     <li><a href="/profil/{{ Auth::getUser()->id }}">{{ Lang::get('auth.profile') }}</a></li>
