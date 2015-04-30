c44cca053ee44bca12cee1f999d8885f476ee4e8
Jimmie <jimmie@define.se>
2015-04-30 11:26:39 +0200

    update race list

--- resources/views/race/list.blade.php
+++ resources/views/race/list.blade.php
@@ -1,12 +1,14 @@
 @if ($races->count() > 0)
 
     @foreach($races as $race)
-        <div class="row" style="border-top: 1px solid #ededed; padding: 20px 0;">
-            <div class="col-lg-2">{{ $race->town }}</div>
-            <div class="col-lg-2">{{ $race->start_datetime }}</div>
-            <div class="col-lg-3">{{ $race->name }}</div>
-            <div class="col-lg-3">{{ ($race->distance / 1000) }} km</div>
-            <div class="col-lg-2 text-right"><a class="btn" href="{{ $race->signup_link }}">Anmäl</a></div>
+        <div class="row list-item">
+            <a href="/race">
+                <div class="col-lg-2 col-md-2 col-xs-2 hide-mobile">{{ $race->town }}</div>
+                <div class="col-lg-2 col-md-2 col-xs-2 hide-mobile">{{ $race->start_datetime }}</div>
+                <div class="col-lg-4 col-md-4 col-xs-9">{{ $race->name }}</div>
+                <div class="col-lg-2 col-md-2 col-xs-3 hide-mobile">{{ round($race->distance / 1000, 1) }} km</div>
+                <div class="col-lg-2 col-md-2 col-xs-3 text-right"><a class="list-btn" href="{{ $race->signup_link }}"><span class="hide-mobile">Läs mer</span> &nbsp; <i class="fa fa-long-arrow-right"></i></a></div>
+            </a>
         </div>
     @endforeach
 
