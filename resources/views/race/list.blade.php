390d57d90d43aa7cbbca2a302150b14297585d7e
Jimmie <jimmie@define.se>
2015-05-05 11:11:03 +0200

    fix läs mer link

--- resources/views/race/list.blade.php
+++ resources/views/race/list.blade.php
@@ -7,7 +7,7 @@
                 <div class="col-lg-2 col-md-2 col-xs-2 hide-mobile">{{ date('Y-m-d', strtotime($race->start_datetime)) }}</div>
                 <div class="col-lg-4 col-md-4 col-xs-9">{{ $race->name }}</div>
                 <div class="col-lg-2 col-md-2 col-xs-3 hide-mobile">{{ round($race->distance / 1000, 1) }} km</div>
-                <div class="col-lg-2 col-md-2 col-xs-3 text-right"><a class="list-btn" href="{{ $race->signup_link }}"><span class="hide-mobile">Läs mer</span> &nbsp; <i class="fa fa-long-arrow-right"></i></a></div>
+                <div class="col-lg-2 col-md-2 col-xs-3 text-right"><a class="list-btn" href="/race/{{ $race->id }}"><span class="hide-mobile">Läs mer</span> &nbsp; <i class="fa fa-long-arrow-right"></i></a></div>
             </a>
         </div>
     @endforeach
