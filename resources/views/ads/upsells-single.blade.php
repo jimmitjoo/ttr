b1c2a72a6a731e317a1abd3f9a9a9bb49efb8880
jespsson <jesper.gunnarsson@gmail.com>
2015-05-08 15:20:25 +0200

    Update upsets + css

--- resources/views/ads/upsells-single.blade.php
+++ resources/views/ads/upsells-single.blade.php
@@ -1,17 +1,18 @@
 <div class="container block-border-bottom block" style="padding-top: 80px; padding-bottom: 60px;">
-
 	<div class="row">
+		
+		<a href="http://track.adtraction.com/t/t?a=435656443&as=1087597488&t=2&tk=1&url=http://www.outnorth.se/2xu/men-s-compression-tight" target="_blank" style="display: block;">
 		<div class="col-lg-6 col-md-6 col-xs-12">
 			<div style="position: relative">
-				<a href="http://track.adtraction.com/t/t?a=435656443&as=1087597488&t=2&tk=1&url=http://www.outnorth.se/2xu/men-s-compression-tight" target="_blank"><img src="images/products/outnorth-2xu-compression-tights.jpg" style="width: 100%; outline: 0;"/></a>
+				<img src="images/products/outnorth-2xu-compression-tights.jpg" style="width: 100%; outline: 0;"/>
 			</div>
 		</div>
 		
 		<div class="col-lg-6 col-md-6 col-xs-12">
 			<div class="table">
 				<div class="table-cell">
-					<h2 class="large">Orka längre med 2XU kompressionskläder</h2>
-					<p>2XU Men's Elite Compression Tights är förmodligen de skönaste löpartightsen du någonsin kommer äga. Stabiliserar de största muskelgrupperna och ger stöd under löpturen.</p>
+					<h2 class="large" style="color: black; margin-bottom: 20px;">Orka längre med 2XU kompressionskläder</h2>
+					<p style="color: black; margin-bottom: 30px;">2XU Men's Elite Compression Tights är förmodligen de skönaste löpartightsen du någonsin kommer äga. Stabiliserar de största muskelgrupperna och ger stöd under löpturen.</p>
 					<div style="position: relative">
 						<div style="position: absolute;">
 							<div><span class="product-label single hide-mobile">2XU Men Compression Tight</span></div>
@@ -22,6 +23,7 @@
 				</div>
 			</div>
 		</div>
+		</a>
 		
 	</div>
 </div>
