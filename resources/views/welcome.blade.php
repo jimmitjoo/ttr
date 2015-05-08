24f4a390348eec488d170c772d7a5d7051319fa5
jespsson <jesper.gunnarsson@gmail.com>
2015-05-08 16:36:47 +0200

    css mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,9 @@
 @extends('master.master')
 
+@section('title', 'Älska att springa - Timetorun.se')
+
+@section('description', 'Hitta lopp och tävlingar att springa i Sverige och utomlands. Löparskor, träningskläder och kompressionskläder till bra pris online.')
+
 @section('content')
 
     <div class="splash">
