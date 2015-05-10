e946f64d4e9d02c64ca203225335cd280dee9d5b
jimmitjoo <jimmitjoo@gmail.com>
2015-05-10 13:10:03 +0200

    change hometown to location

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,7 +32,7 @@ Route::get('partials/race-list', 'OrganizersController@listRaces');
 Route::get('profil/{id}', ['as' => 'profile', 'uses' => 'UsersController@show']);
 
 Route::get('login/facebook', function(){
-    return Socialize::with('facebook')->scopes(['email', 'hometown'])->redirect();
+    return Socialize::with('facebook')->scopes(['email', 'location'])->redirect();
 });
 
 Route::get('receive/facebook', function(){
