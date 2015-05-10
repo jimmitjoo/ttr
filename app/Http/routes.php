85cc3b07cc59da13659cdbe2b6acc4410b60e660
jimmitjoo <jimmitjoo@gmail.com>
2015-05-10 13:10:38 +0200

    just email again

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -32,7 +32,7 @@ Route::get('partials/race-list', 'OrganizersController@listRaces');
 Route::get('profil/{id}', ['as' => 'profile', 'uses' => 'UsersController@show']);
 
 Route::get('login/facebook', function(){
-    return Socialize::with('facebook')->scopes(['email', 'location'])->redirect();
+    return Socialize::with('facebook')->scopes(['email'])->redirect();
 });
 
 Route::get('receive/facebook', function(){
