f8edfbe3723fda516dea97a3bfd9f24cc20ce44f
Jimmie <jimmie@define.se>
2015-05-11 14:39:14 +0200

    fix runtitle with townname if it isnt in the title already

--- app/Http/Controllers/RunsController.php
+++ app/Http/Controllers/RunsController.php
@@ -69,7 +69,12 @@ class RunsController extends Controller {
 
         $lastSpacePosition = strrpos($run->name, ' ');
         $run->purename = substr($run->name, 0, $lastSpacePosition);
-        $run->title = $run->purename . ', ' . $run->town;
+
+        if (!strpos($run->purename, $run->town)) {
+            $run->title = $run->purename . ', ' . $run->town;
+        } else {
+            $run->title = $run->purename;
+        }
 
         $races = Run::where('town', '=', $run->town)->with('organizer')->paginate();
 
