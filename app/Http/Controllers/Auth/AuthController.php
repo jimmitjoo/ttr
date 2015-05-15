24d180c452c2ba86a90e602fccf0ed3728d2f48c
Jimmie <jimmie@define.se>
2015-05-15 16:33:29 +0200

    update login redirect

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -20,7 +20,7 @@ class AuthController extends Controller {
 
 	use AuthenticatesAndRegistersUsers;
 
-    protected $redirectTo = '/';
+    protected $redirectPath = '/';
 
 	/**
 	 * Create a new authentication controller instance.
