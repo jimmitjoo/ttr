c7b1423125dbab22c0c110923b380262a327a5ba
jespsson <jesper.gunnarsson@gmail.com>
2015-05-20 13:56:35 +0200

    merge

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,9 +8,9 @@
 
     <div class="max-width container-fluid block bg-white">
         <div class="row">
-            <div class="col-lg-12">
-            	<h1 class="big blue">{{ Lang::get('default.welcome') }} {{ $user->name }}!</h1>
-            	<p>Här kan du ändra dina uppgifter och lägga till information.</p>
+            <div class="col-lg-8">
+            	<h1 class="big blue title">{{ Lang::get('default.welcome') }}<!--{{ $user->name }}-->!</h1>
+            	<p>Detta är din dashboard på Timetorun. Härifrån kan du ändra dina uppgifter, lägga till information skapa lopp och träningspass som andra kan registrerar sig till.</p>
                 <div>{{ Lang::get('default.you_are_signed_in') }}. <a href="/lopp/skapa">{{ Lang::get('race.create') }}</a>.</div>
             </div>
         </div>
